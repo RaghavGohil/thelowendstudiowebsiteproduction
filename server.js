a343ff10958a594303aaf372ee1a1534d889d4c4
RaghavGohil <raghav15102004@gmail.com>
2023-06-24 17:23:53 +0530

    config update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 const path = require('path')
 const { urlencoded } = require('express')
 const app = express()
-const port = process.enc.PORT || 3001
+const port = process.env.PORT || 3001
 
 const faviconPath = '/public/images/favicon/favicon.ico'
 const publicPath = '/public'
